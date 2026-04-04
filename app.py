cfde71d293266100c25297c1c316b7073bcd6a39
Nquartey17 <niikwarteiq@gmail.com>
2026-04-04 18:32:24 -0400

    Testing backend with postman before starting frontend, file uploads working

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ def home():
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
-    file = request.files["file"]
+    file = request.files.get("test")
+
+    #If file doesn't exist, prevent crashing
+    if not file:
+        return {"error": "File missing"}, 400
+
     content = file.read().decode("utf-8")
 
     results = text_scan(content)
